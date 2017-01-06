2d05fdfb8b95ef20b125221b9ba62961f36cd6d9
mohandossKrishan <mohandoss28@gmail.com>
2017-01-06 16:32:26 +0530

    ACD_JAVAB2_Session_3_Assignment_3_Main

---
+++ Example.java
@@ -0,0 +1,9 @@
+class Example {
+    int x; int y;
+    String name;
+
+    public static void main(String args[]) {
+        Example pnt = new Example();
+        System.out.println("pnt is " + pnt.name + " " + pnt.x + " " + pnt.y);
+       }
+}
